1046697f04dc5399923e5b89f868915fc3b4cff5
marsczen <pealstyle@gmail.com>
2023-03-11 12:39:59 +0800

    feat: rm sidebar footer

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -3,7 +3,6 @@ import type { CSSProperties } from 'vue'
 import { computed, watch } from 'vue'
 import { NButton, NLayoutSider } from 'naive-ui'
 import List from './List.vue'
-import Footer from './Footer.vue'
 import { useAppStore, useChatStore } from '@/store'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 
@@ -76,7 +75,6 @@ watch(
           <List />
         </div>
       </main>
-      <Footer />
     </div>
   </NLayoutSider>
   <template v-if="isMobile">
