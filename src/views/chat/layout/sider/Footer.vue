935fbe9be4d033abccfd73a8e57be3f15c709a8f
czen <czen@192.168.0.102>
2023-03-29 01:31:51 +0800

    feat: initial commit

---
+++ src/views/chat/layout/sider/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+import { defineAsyncComponent, ref } from 'vue'
+
+const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
+
+const show = ref(false)
+</script>
+
+<template>
+  <footer class="flex items-center justify-between min-w-0 p-4 overflow-hidden border-t dark:border-neutral-800">
+    <Setting v-if="show" v-model:visible="show" />
+  </footer>
+</template>
