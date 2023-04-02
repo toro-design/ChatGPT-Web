bf91f5cef9228022f4e12962eb927950f4b6083a
marsczen <pealstyle@gmail.com>
2023-04-02 20:49:22 +0800

    chore: update avatar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://avatars.githubusercontent.com/u/7029383?v=4',
+      avatar: 'https://thumbnail0.baidupcs.com/thumbnail/e09e6f684p2e90aa8cdbfbe309d190dc?fid=422677245-250528-976771319875639&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-BpSFqGuBdYkJkovFEKqm6H%2b8lps%3d&expires=8h&chkbd=0&chkv=0&dp-logid=9048175788993167999&dp-callid=0&time=1680436800&size=c1999_u1999&quality=100&vuk=422677245&ft=image',
       name: 'bot',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
